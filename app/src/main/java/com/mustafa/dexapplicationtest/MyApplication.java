7bf972b04b53a4ae7fd2c33329398f1813e7d9db
Mustafa Ali <mustafa.ali@mutualmobile.com>
2014-10-23 15:20:21 +0530

    Updated instructions on how to enable multi-dexing

--- app/src/main/java/com/mustafa/dexapplicationtest/MyApplication.java
+++ app/src/main/java/com/mustafa/dexapplicationtest/MyApplication.java
@@ -1,7 +1,5 @@
 package com.mustafa.dexapplicationtest;
 
-import android.content.Context;
-import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
 /**
@@ -9,9 +7,18 @@ import android.support.multidex.MultiDexApplication;
  */
 public class MyApplication extends MultiDexApplication {
 
+    /* To enable Multi-Dex, you have 3 options -
+        1. Declare android.support.multidex.MultiDexApplication as your application class in AndroidManifest.xml
+        2. Extend MultiDexApplication like we are doing here
+        3. Override attachBaseContext() in your application class as shown below
+    /*
+
+     */
+    /*
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
     }
+    */
 }
